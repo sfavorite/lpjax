a829cd096fd740fbe308b18050765bba96d74ab0
Scot Favorite <scot.favorite@gmail.com>
2019-11-01 10:41:54 -0400

    Updated nav to bootstrap 4

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,21 +1,18 @@
-<div class="navbar navbar-inverse navbar-fixed-top navbar-custom">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a href='/' class='navbar-brand'>Pjax - Laravel</a>
-    </div>
-    <div class="collapse navbar-collapse">
-      <ul class="nav navbar-nav">
-        <li  class='{{ $active == "home" ? "active" : '' }}'><a href="/" data-pjax='#main'>Home</a></li>
-        <li class='{{ $active == "about" ? "active" : '' }}'><a href="/about" data-pjax='#main'>About</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#" >Guest</a></li>
-      </ul>
-    </div><!--/.nav-collapse -->
-  </div>
-</div>
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <!-- Brand -->
+        <a href='/' class='navbar-brand'>Pjax - Laravel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavBar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavBar">
+            <!-- Links -->
+            <ul class="nav navbar-nav mr-auto">
+                <li  class='{{ $active == "home" ? "active" : '' }} nav-item'><a href="/" data-pjax='#main' class='nav-link'>Home</a></li>
+                <li class='{{ $active == "about" ? "active" : '' }} nav-item'><a href="/about" data-pjax='#main' class='nav-link'>About</a></li>
+            </ul>
+            <ul class="nav navbar-nav ml-auto">
+              <li class='nav-item'><a class="nav-link" href="#" >Guest</a></li>
+            </ul>
+
+        </div>
+</nav>
